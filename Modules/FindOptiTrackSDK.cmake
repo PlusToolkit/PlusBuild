a785ac95abfbd4dc1994fb9211370ab2df212311
Andras Lasso <lasso@queensu.ca>
2017-07-12 13:19:31 -0400

    re #1208: Added path hint to find NatNed SDK in PLTools

--- Modules/FindOptiTrackSDK.cmake
+++ Modules/FindOptiTrackSDK.cmake
@@ -12,11 +12,16 @@
 #  OPTITRACKSDK_BINARY_DIR           Binaries needed to use NatNet SDK
 
 SET(OPTITRACKSDK_PATH_HINTS
-  "$ENV{PROGRAMFILES}/OptiTrack/NatNetSDK" 
+  "$ENV{PROGRAMFILES}/OptiTrack/NatNetSDK"
   "$ENV{PROGRAMW6432}/OptiTrack/NatNetSDK"
   "C:/Program Files (x86)/OptiTrack/NatNetSDK"
-  "C:/Program Files/OptiTrack/NatNetSDK")
-  
+  "C:/Program Files/OptiTrack/NatNetSDK"
+  "../PLTools/OptiTrack/NatNet-2.10"
+  "../../PLTools/OptiTrack/NatNet-2.10"
+  "../trunk/PLTools/OptiTrack/NatNet-2.10"
+  "${CMAKE_CURRENT_BINARY_DIR}/PLTools/OptiTrack/NatNet-2.10"
+  )
+
 find_path(OPTITRACKSDK_DIR Readme-NatNet.txt
   PATHS ${OPTITRACKSDK_PATH_HINTS}
   DOC "OptiTrack NatNet SDK directory")
@@ -25,32 +30,32 @@ if (OPTITRACKSDK_DIR)
   # Include directories
   set(OPTITRACKSDK_INCLUDE_DIR ${OPTITRACKSDK_DIR}/include)
   mark_as_advanced(OPTITRACKSDK_INCLUDE_DIR)
-  
+
   # Libraries
   SET(PLATFORM_SUFFIX "")
   IF (CMAKE_HOST_WIN32 AND CMAKE_CL_64 )
     SET(PLATFORM_SUFFIX "/x64/")
   ENDIF (CMAKE_HOST_WIN32 AND CMAKE_CL_64 )
-  
+
   find_path(OptiTrackSDK_LIBRARY_DIR
             NAMES NatNetLib${CMAKE_STATIC_LIBRARY_SUFFIX}
             PATHS "${OPTITRACKSDK_DIR}/lib" NO_DEFAULT_PATH
             PATH_SUFFIXES ${PLATFORM_SUFFIX})
-  
+
   find_path(OptiTrackSDK_BINARY_DIR
             NAMES NatNetLib${CMAKE_SHARED_LIBRARY_SUFFIX}
             PATHS "${OPTITRACKSDK_DIR}/lib" NO_DEFAULT_PATH
             PATH_SUFFIXES ${PLATFORM_SUFFIX})
-        
+
   set(OPTITRACKSDK_LIBRARY_DIR ${OptiTrackSDK_LIBRARY_DIR})
   set(OPTITRACKSDK_BINARY_DIR ${OptiTrackSDK_BINARY_DIR})
   mark_as_advanced(OptiTrackSDK_LIBRARY_DIR)
   mark_as_advanced(OptiTrackSDK_BINARY_DIR)
-  
+
   #Version
   #TODO: properly set SDK version using REGEX from NatNetTypes.h
   set(OPTITRACKSDK_VERSION "2.10.0")
- 
+
 endif()
 
 include(FindPackageHandleStandardArgs)
