8354d653f2a4944394e1fbc806b9b31ec626e356
Adam Rankin <adam.rankin@gmail.com>
2017-09-18 16:05:33 -0400

    BUG: Locking version of openigtlinkio until pull request for new utility function is accepted in pluslib

--- SuperBuild/External_OpenIGTLinkIO.cmake
+++ SuperBuild/External_OpenIGTLinkIO.cmake
@@ -38,7 +38,7 @@ ELSE()
     BINARY_DIR "${PLUS_OpenIGTLinkIO_DIR}"
     #--Download step--------------
     GIT_REPOSITORY "${GIT_PROTOCOL}://github.com/IGSIO/OpenIGTLinkIO.git"
-    GIT_TAG master
+    GIT_TAG 774196b9b097b69fb04b311debb649dd9e921512
     #--Configure step-------------
     CMAKE_ARGS 
       ${ep_common_args}
