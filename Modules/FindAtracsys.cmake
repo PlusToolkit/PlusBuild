1782f67f35dcd1784fe832620bb63c03c643b5c6
Simon Drouin <drouin.simon@gmail.com>
2017-12-21 16:02:22 -0500

    CMake can now find the Atracsys SDK on Linux.

--- Modules/FindAtracsys.cmake
+++ Modules/FindAtracsys.cmake
@@ -22,9 +22,13 @@ SET(ATRACSYS_SDK_PATH_HINTS
   "${CMAKE_CURRENT_BINARY_DIR}/PLTools/Atracsys/sTk Passive Tracking SDK"
   )
 
-find_path(ATRACSYS_SDK_DIR versions.txt
-  PATHS ${ATRACSYS_SDK_PATH_HINTS}
-  DOC "Atracsys sTk Passive Tracking SDK directory")
+if( WIN32 )
+    find_path(ATRACSYS_SDK_DIR versions.txt
+        PATHS ${ATRACSYS_SDK_PATH_HINTS}
+        DOC "Atracsys sTk Passive Tracking SDK directory")
+else()
+    find_path( ATRACSYS_SDK_DIR include/ftkTypes.h DOC "Directory where ftkTypes.h is located" )
+endif()
 
 if (ATRACSYS_SDK_DIR)
   # Include directories
@@ -32,14 +36,21 @@ if (ATRACSYS_SDK_DIR)
   mark_as_advanced(ATRACSYS_SDK_INCLUDE_DIR)
 
   # Libraries
-  find_path(AtracsysSDK_LIBRARY_DIR
+  if( WIN32 )
+    find_path(AtracsysSDK_LIBRARY_DIR
             NAMES fusionTrack32${CMAKE_STATIC_LIBRARY_SUFFIX}
             PATHS "${ATRACSYS_SDK_DIR}/lib" NO_DEFAULT_PATH)
 
-  find_path(AtracsysSDK_BINARY_DIR
+    find_path(AtracsysSDK_BINARY_DIR
             NAMES fusionTrack32${CMAKE_SHARED_LIBRARY_SUFFIX}
             PATHS "${ATRACSYS_SDK_DIR}/bin" NO_DEFAULT_PATH)
-            
+  else()
+    find_library( AtracsysSDK_LIBRARY_DIR
+            NAMES fusionTrack32 
+            PATHS "${ATRACSYS_SDK_DIR}/lib" NO_DEFAULT_PATH )
+    set( AtracsysSDK_BINARY_DIR ${AtracsysSDK_LIBRARY_DIR} )
+  endif()
+
   set(ATRACSYS_SDK_LIBRARY_DIR ${AtracsysSDK_LIBRARY_DIR})
   set(ATRACSYS_SDK_BINARY_DIR ${AtracsysSDK_BINARY_DIR})
   mark_as_advanced(AtracsysSDK_LIBRARY_DIR)
