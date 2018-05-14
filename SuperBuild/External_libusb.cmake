6af682858809bbcb27bb87554dbdb9ab941cf8b8
Jorge Sierra Acosta <jsierra.siete@gmail.com>
2018-05-14 14:34:11 +0100

    Added comments to libusb external file
    Removed wrong library from variable

--- SuperBuild/External_libusb.cmake
+++ SuperBuild/External_libusb.cmake
@@ -10,17 +10,20 @@ IF (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
   SET(LibUSB_PREFIX   ${DEPS_DIR}/libusb-prefix)
   SET(LibUSB_ROOT_DIR ${LibUSB_SRC})
 
+  # PATHS WHERE WE KNOW THE HEADERS WILL BE LOCATED
   SET(LibUSB_INCLUDE_DIRS ${LibUSB_SRC}/include)
   SET(LibUSB_INCLUDES     ${LibUSB_SRC}/include)
   SET(LIBUSB_INCLUDE_DIR  ${LibUSB_SRC}/include)
 
+  # PATH WHERE WE KNOW THE LIBRARY WILL BE LOCATED
   IF(MSVC OR ${CMAKE_GENERATOR} MATCHES "Xcode")
     SET(LibUSB_LIBRARY_DIR  ${LibUSB_SRC}/MS32/dll)
   ELSE ()
     SET(LibUSB_LIBRARY_DIR  ${LibUSB_SRC}/MinGW32/dll)
   ENDIF ()
-  SET(LIBUSB_LIBRARY ${LibUSB_LIBRARY_DIR}/libusb-1.0.dll ${LibUSB_LIBRARY_DIR}/libusb-1.0.lib)
+  SET(LIBUSB_LIBRARY ${LibUSB_LIBRARY_DIR}/libusb-1.0.lib)
 
+  # DOWNLOAD LIBUSB
   INCLUDE(ExternalProject)
   ExternalProject_Add(LibUSB
     PREFIX     ${LibUSB_PREFIX}
@@ -38,6 +41,7 @@ IF (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 
 ELSE ()
 
+  # LIBUSB LOCATION IF INSTALLED IN LINUX
   # Copied from original repository https://github.com/maartenvds/libseek-thermal
   SET(LibUSB_INCLUDE_DIRS /usr/include/LibUSB-1.0)
   SET(LibUSB_INCLUDES     /usr/include/LibUSB-1.0)
