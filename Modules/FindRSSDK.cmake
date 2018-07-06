7213b8444218e68b49337127de550f0784db44f6
Mark Asselin <mark.asselin95@gmail.com>
2018-07-06 16:57:20 -0400

    re PlusToolkit/PlusLib#253: Updated Intel RealSense support to latest version of librealsense, streamlined codebase to have only one Intel RealSense device

--- Modules/FindRSSDK.cmake
+++ Modules/FindRSSDK.cmake
@@ -1,72 +1,69 @@
 ###############################################################################
-# Find Intel RealSense SDK
+# Find Intel RealSense SDK 2.0
 #
 #     find_package(RSSDK)
 #
 # Variables defined by this module:
 #
 #  RSSDK_FOUND                 True if RealSense SDK was found
-#  RSSDK_VERSION               The version of RealSense SDK
 #  RSSDK_INCLUDE_DIRS          The location(s) of RealSense SDK headers
 #  RSSDK_LIBRARIES             Libraries needed to use RealSense SDK
 
-find_path(RSSDK_DIR include/pxcversion.h
-          PATHS "$ENV{RSSDK_DIR}"
-                "$ENV{PROGRAMFILES}/Intel/RSSDK"
-                "$ENV{PROGRAMW6432}/Intel/RSSDK"
-                "C:/Program Files (x86)/Intel/RSSDK"
-                "C:/Program Files/Intel/RSSDK"
-          DOC "RealSense SDK directory")
+SET(RSSDK_PATHS
+PATHS
+  "$ENV{PROGRAMFILES}/Intel RealSense SDK 2.0"
+  "$ENV{PROGRAMW6432}/Intel RealSense SDK 2.0"
+  "C:/Program Files (x86)/Intel RealSense SDK 2.0"
+  "C:/Program Files/Intel RealSense SDK 2.0"
+  ../PLTools/IntelRealSense
+  ../../PLTools/IntelRealSense
+  ../PLTools/trunk/IntelRealSense
+)
+find_path(RSSDK_DIR include/librealsense2/rsutil.h
+  PATHS ${RSSDK_PATHS})
 
 if(RSSDK_DIR)
-
   # Include directories
-  set(RSSDK_INCLUDE_DIRS ${RSSDK_DIR}/include)
-  mark_as_advanced(RSSDK_INCLUDE_DIRS)
-
+  set(RSSDK_INCLUDE_DIR ${RSSDK_DIR}/include/librealsense2 CACHE FILEPATH "RealSense header files directory.")
+  mark_as_advanced(RSSDK_INCLUDE_DIR)
+  
   # Libraries
-  set(RSSDK_RELEASE_NAME libpxc.lib)
-  set(RSSDK_DEBUG_NAME libpxc_d.lib)
-
-  SET(PLATFORM_SUFFIX Win32)
+  set(RSSDK_LIB_NAME realsense2${CMAKE_STATIC_LIBRARY_SUFFIX})
+  set(RSSDK_BIN_NAME realsense2${CMAKE_SHARED_LIBRARY_SUFFIX})
+  
+  SET(PLATFORM_SUFFIX "x86")
   IF (CMAKE_HOST_WIN32 AND CMAKE_CL_64 )
     SET(PLATFORM_SUFFIX "x64")
   ENDIF (CMAKE_HOST_WIN32 AND CMAKE_CL_64 )
 
   find_library(RSSDK_LIBRARY
-               NAMES ${RSSDK_RELEASE_NAME}
-               PATHS "${RSSDK_DIR}/lib/" NO_DEFAULT_PATH
+               NAMES ${RSSDK_LIB_NAME}
+               PATHS "${RSSDK_DIR}/lib/${PLATFORM_SUFFIX}/" NO_DEFAULT_PATH
                PATH_SUFFIXES ${PLATFORM_SUFFIX})
-  find_library(RSSDK_LIBRARY_DEBUG
-               NAMES ${RSSDK_DEBUG_NAME} ${RSSDK_RELEASE_NAME}
-               PATHS "${RSSDK_DIR}/lib/" NO_DEFAULT_PATH
+  
+  find_path(RSSDK_BINARY_DIR
+               NAMES ${RSSDK_BIN_NAME}
+               PATHS "${RSSDK_DIR}/bin/${PLATFORM_SUFFIX}/" NO_DEFAULT_PATH
                PATH_SUFFIXES ${PLATFORM_SUFFIX})
-  if(NOT RSSDK_LIBRARY_DEBUG)
-    set(RSSDK_LIBRARY_DEBUG ${RSSDK_LIBRARY})
-  endif()
-  set(RSSDK_LIBRARIES optimized ${RSSDK_LIBRARY} debug ${RSSDK_LIBRARY_DEBUG})
-  mark_as_advanced(RSSDK_LIBRARY RSSDK_LIBRARY_DEBUG)
 
+  set(RSSDK_LIB ${RSSDK_LIBRARY})
+  mark_as_advanced(RSSDK_LIB)
+  set(RSSDK_BIN ${RSSDK_BINARY_DIR}/${RSSDK_BIN_NAME})
+  mark_as_advanced(RSSDK_BIN)
+  
   # Version
-  set(RSSDK_VERSION 0)
-  file(STRINGS "${RSSDK_INCLUDE_DIRS}/pxcversion.h" _pxcversion_H_CONTENTS REGEX "#define PXC_VERSION_.*")
-  set(_RSSDK_VERSION_REGEX "([0-9]+)")
-  foreach(v MAJOR MINOR BUILD REVISION)
-    if("${_pxcversion_H_CONTENTS}" MATCHES ".*#define PXC_VERSION_${v} *${_RSSDK_VERSION_REGEX}.*")
-      set(RSSDK_VERSION_${v} "${CMAKE_MATCH_1}")
-    endif()
-  endforeach()
-  unset(_pxcversion_H_CONTENTS)
-  set(RSSDK_VERSION "${RSSDK_VERSION_MAJOR}.${RSSDK_VERSION_MINOR}.${RSSDK_VERSION_BUILD}.${RSSDK_VERSION_REVISION}")
-
-endif()
+  set(RSSDK_VERSION "2.0")
+  
+endif()  
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(RSSDK
   FOUND_VAR RSSDK_FOUND
-  REQUIRED_VARS RSSDK_LIBRARIES RSSDK_INCLUDE_DIRS
+  REQUIRED_VARS RSSDK_LIB RSSDK_BIN RSSDK_INCLUDE_DIR
   VERSION_VAR RSSDK_VERSION
 )
+
+ADD_LIBRARY(RSSDK SHARED IMPORTED)
 
 if(MSVC)
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /NODEFAULTLIB:LIBCMTD")
