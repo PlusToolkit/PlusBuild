89f9eba243ba7362064020f77fd4e4f69ba93133
Mark Asselin <mark@pixelmedical.ca>
2021-04-09 22:58:44 -0500

    Ensure MotiveAPI and NatNetSDK find_package_handle_standard_args package name follows the CMake naming conventions

--- Modules/FindMotiveAPI.cmake
+++ Modules/FindMotiveAPI.cmake
@@ -5,16 +5,13 @@
 #
 # Variables defined by this module:
 #
-#  MOTIVE_API_FOUND               True if OptiTrack Motive API was found
-#  MOTIVE_API_VERSION             The version of Motive API
-#  MOTIVE_API_INCLUDE_DIR         The location(s) of Motive API headers
-#  MOTIVE_API_LIBRARY_DIR         Libraries needed to use Motive API
-#  MOTIVE_API_BINARY_DIR          Binaries needed to use Motive API
-#  MOTIVE_API_LIBRARY_NAME        Name of Motive API library file
-#  MOTIVE_API_BINARY_NAME         Name of Motive API binary file
-#  MOTIVE_OPENMP_DIR              Location of libiomp5md.dll needed to use Motive API
+#  MotiveAPI_FOUND                True if OptiTrack Motive API was found
+#  MotiveAPI_VERSION              The version of Motive API
+#  MotiveAPI_DIR                  The directory containing Motive API
+#  MotiveAPI_INCLUDE_DIR          The location(s) of Motive API headers
+#  MotiveAPI_MSVC80_OpenMP_DIR    Location of libiomp5md.dll needed to use Motive API
 
-SET(MOTIVEAPI_PATH_HINTS
+SET(MotiveAPI_PATH_HINTS
   "../PLTools/OptiTrack/MotiveAPI-${MOTIVE_VERSION}"
   "../../PLTools/OptiTrack/MotiveAPI-${MOTIVE_VERSION}"
   "../trunk/PLTools/OptiTrack/MotiveAPI-${MOTIVE_VERSION}"
@@ -32,29 +29,25 @@ SET(Microsoft_VC80_OpenMP_PATH_HINTS
   "${CMAKE_CURRENT_BINARY_DIR}/PLTools/OptiTrack/Microsoft.VC80.OpenMP"
   )
 
-find_path(MOTIVE_DIR "/inc/NPTrackingTools.h"
-  PATHS ${MOTIVEAPI_PATH_HINTS}
+find_path(MotiveAPI_DIR "/inc/NPTrackingTools.h"
+  PATHS ${MotiveAPI_PATH_HINTS}
   DOC "OptiTrack Motive API directory")
 
-find_path(MOTIVE_MSVC80_OpenMP_DIR
+find_path(MotiveAPI_MSVC80_OpenMP_DIR
       NAMES vcomp.dll
       PATHS ${Microsoft_VC80_OpenMP_PATH_HINTS}
       DOC "Microsoft VC80 OpenMP directory")
 
-IF (MOTIVE_DIR)
-  # Include directories
-  set(MOTIVE_API_INCLUDE_DIR ${MOTIVE_DIR}/inc)
-  mark_as_advanced(MOTIVE_API_INCLUDE_DIR)
+IF (MotiveAPI_DIR)
+  set(MotiveAPI_INCLUDE_DIR ${MotiveAPI_DIR}/inc)
+  mark_as_advanced(MotiveAPI_INCLUDE_DIR)
 
-  #Version
-  #TODO: properly set SDK version using REGEX
-  set(MOTIVE_API_VERSION ${MOTIVE_VERSION})
-
+  set(MotiveAPI_VERSION ${MOTIVE_VERSION})
 ENDIF ()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MOTIVE_API
-  FOUND_VAR MOTIVE_API_FOUND
-  REQUIRED_VARS MOTIVE_API_INCLUDE_DIR MOTIVE_DIR
-  VERSION_VAR MOTIVE_API_VERSION
+find_package_handle_standard_args(MotiveAPI
+  FOUND_VAR MotiveAPI_FOUND
+  REQUIRED_VARS MotiveAPI_INCLUDE_DIR MotiveAPI_DIR
+  VERSION_VAR MotiveAPI_VERSION
 )
--- Modules/FindNatNetSDK.cmake
+++ Modules/FindNatNetSDK.cmake
@@ -5,13 +5,13 @@
 #
 # Variables defined by this module:
 #
-#  NATNET_SDK_FOUND                True if OptiTrack NatNet SDK was found
-#  NATNET_SDK_VERSION              The version of NatNet SDK
-#  NATNET_SDK_INCLUDE_DIR          The location(s) of NatNet SDK headers
-#  NATNET_SDK_LIBRARY_DIR          Libraries needed to use NatNet SDK
-#  NATNET_SDK_BINARY_DIR           Binaries needed to use NatNet SDK
+#  NatNetSDK_FOUND                True if OptiTrack NatNet SDK was found
+#  NatNetSDK_VERSION              The version of NatNet SDK
+#  NatNetSDK_INCLUDE_DIR          The location(s) of NatNet SDK headers
+#  NatNetSDK_LIBRARY_DIR          Libraries needed to use NatNet SDK
+#  NatNetSDK_BINARY_DIR           Binaries needed to use NatNet SDK
 
-SET(NATNET_SDK_PATH_HINTS
+SET(NatNetSDK_PATH_HINTS
   "../PLTools/OptiTrack/NatNet-3.1"
   "../../PLTools/OptiTrack/NatNet-3.1"
   "../trunk/PLTools/OptiTrack/NatNet-3.1"
@@ -22,14 +22,14 @@ SET(NATNET_SDK_PATH_HINTS
   "C:/Program Files/OptiTrack/NatNetSDK"
   )
 
-find_path(NATNET_SDK_DIR Readme-NatNet.txt
-  PATHS ${NATNET_SDK_PATH_HINTS}
+find_path(NatNetSDK_DIR Readme-NatNet.txt
+  PATHS ${NatNetSDK_PATH_HINTS}
   DOC "OptiTrack NatNet SDK directory")
 
-if (NATNET_SDK_DIR)
+if (NatNetSDK_DIR)
   # Include directories
-  set(NATNET_SDK_INCLUDE_DIR ${NATNET_SDK_DIR}/include)
-  mark_as_advanced(NATNET_SDK_INCLUDE_DIR)
+  set(NatNetSDK_INCLUDE_DIR ${NatNetSDK_DIR}/include)
+  mark_as_advanced(NatNetSDK_INCLUDE_DIR)
 
   # Libraries
   SET(PLATFORM_SUFFIX "/x86/")
@@ -39,28 +39,28 @@ if (NATNET_SDK_DIR)
 
   find_path(NatNetSDK_LIBRARY_DIR
             NAMES NatNetLib${CMAKE_STATIC_LIBRARY_SUFFIX}
-            PATHS "${NATNET_SDK_DIR}/lib" NO_DEFAULT_PATH
+            PATHS "${NatNetSDK_DIR}/lib" NO_DEFAULT_PATH
             PATH_SUFFIXES ${PLATFORM_SUFFIX})
 
   find_path(NatNetSDK_BINARY_DIR
             NAMES NatNetLib${CMAKE_SHARED_LIBRARY_SUFFIX}
-            PATHS "${NATNET_SDK_DIR}/lib" NO_DEFAULT_PATH
+            PATHS "${NatNetSDK_DIR}/lib" NO_DEFAULT_PATH
             PATH_SUFFIXES ${PLATFORM_SUFFIX})
 
-  set(NATNET_SDK_LIBRARY_DIR ${NatNetSDK_LIBRARY_DIR})
-  set(NATNET_SDK_BINARY_DIR ${NatNetSDK_BINARY_DIR})
+  set(NatNetSDK_LIBRARY_DIR ${NatNetSDK_LIBRARY_DIR})
+  set(NatNetSDK_BINARY_DIR ${NatNetSDK_BINARY_DIR})
   mark_as_advanced(NatNetSDK_LIBRARY_DIR)
   mark_as_advanced(NatNetSDK_BINARY_DIR)
 
   #Version
   #TODO: properly set SDK version using REGEX from NatNetTypes.h
-  set(NATNET_SDK_VERSION "3.1")
+  set(NatNetSDK_VERSION "3.1")
 
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NATNET_SDK
-  FOUND_VAR NATNET_SDK_FOUND
-  REQUIRED_VARS NATNET_SDK_LIBRARY_DIR NATNET_SDK_BINARY_DIR NATNET_SDK_INCLUDE_DIR
-  VERSION_VAR NATNET_SDK_VERSION
+find_package_handle_standard_args(NatNetSDK
+  FOUND_VAR NatNetSDK_FOUND
+  REQUIRED_VARS NatNetSDK_INCLUDE_DIR NatNetSDK_LIBRARY_DIR NatNetSDK_BINARY_DIR
+  VERSION_VAR NatNetSDK_VERSION
 )
