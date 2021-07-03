79507f55a2892d5fe404a2de15fe93cb4dbcf21f
Adam Rankin <adam.rankin@gmail.com>
2021-07-03 09:10:05 -0400

    Improve detection of OpenVR in PlusLib when building from PlusBuild

--- SuperBuild/External_OpenVR.cmake
+++ SuperBuild/External_OpenVR.cmake
@@ -7,7 +7,16 @@ IF(OpenVR_DIR)
   # Copy libraries to CMAKE_RUNTIME_OUTPUT_DIRECTORY
   PlusCopyLibrariesToDirectory(${CMAKE_RUNTIME_OUTPUT_DIRECTORY} OpenVR)
 
-  SET (PLUS_OVR_DIR ${OpenVR_DIR} CACHE INTERNAL "Path to store OpenVR binaries")
+  SET (PLUS_OVR_DIR ${OpenVR_DIR} CACHE INTERNAL "Path to store OpenVR SDK")
+ELSEIF(OPENVR_ROOT_DIR)
+  FIND_PACKAGE(OpenVR REQUIRED)
+
+  MESSAGE(STATUS "Using OpenVR available at: ${OpenVR_DIR}")
+
+  # Copy libraries to CMAKE_RUNTIME_OUTPUT_DIRECTORY
+  PlusCopyLibrariesToDirectory(${CMAKE_RUNTIME_OUTPUT_DIRECTORY} OpenVR)
+
+  SET (PLUS_OVR_DIR ${OPENVR_ROOT_DIR} CACHE INTERNAL "Path to store OpenVR SDK")
 ELSE()
   # OpenVR has not been built yet, so download and build it as an external project
   SetGitRepositoryTag(
@@ -17,7 +26,7 @@ ELSE()
     )
 
   SET (PLUS_OpenVR_SRC_DIR "${CMAKE_BINARY_DIR}/OpenVR")
-  SET (PLUS_OpenVR_DIR "${CMAKE_BINARY_DIR}/OpenVR" CACHE INTERNAL "Path to store OpenVR binaries")
+  SET (PLUS_OpenVR_DIR "${CMAKE_BINARY_DIR}/OpenVR" CACHE INTERNAL "Path to store OpenVR SDK")
   ExternalProject_Add( OpenVR
     "${PLUSBUILD_EXTERNAL_PROJECT_CUSTOM_COMMANDS}"
     PREFIX "${CMAKE_BINARY_DIR}/Deps/OpenVR-prefix"
@@ -33,4 +42,6 @@ ELSE()
     INSTALL_COMMAND ""
     DEPENDS ${OpenVR_DEPENDENCIES}
     )
+
+  SET(PLUS_OVR_DIR ${PLUS_OpenVR_SRC_DIR} CACHE INTERNAL "Path to store OpenVR SDK")
 ENDIF()
