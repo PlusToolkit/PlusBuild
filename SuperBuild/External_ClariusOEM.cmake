69703205c832fc01d537b65745575bbda9eed246
Kyle Sunderland <sunderlandkyl@gmail.com>
2023-02-22 11:54:24 -0500

    ENH: Update Clarius OEM to 10.2.2

--- SuperBuild/External_ClariusOEM.cmake
+++ SuperBuild/External_ClariusOEM.cmake
@@ -17,7 +17,7 @@ ELSE()
   SetGitRepositoryTag(
     ClariusOEM
     "https://github.com/clariusdev/solum.git"
-    "v10.1.1"
+    "v10.2.2"
     )
 
   SET(ClariusOEM_OUTER_SRC_DIR "${CMAKE_BINARY_DIR}/ClariusOEM")
@@ -42,8 +42,8 @@ ELSE()
 
   # download zipped lib/dll files
 
-  SET(CLARIUS_OEM_PACKAGE_URL "https://github.com/clariusdev/solum/releases/download/v10.1.1/solum-10.1.1-windows.zip")
-  SET(CLARIUS_OEM_PACKAGE_SHA256 "3d5f365ad1894d4d0ea226258e5b6440e9d338f191f521fb96c27dca3610f94b")
+  SET(CLARIUS_OEM_PACKAGE_URL "https://github.com/clariusdev/solum/releases/download/v10.2.2/solum-10.2.2-windows.zip")
+  SET(CLARIUS_OEM_PACKAGE_SHA256 "93eff96fde4a1eb3cee710bb13544980986444d78340527e17004508d4e568ad")
 
   ExternalProject_Add(ClariusOEM-Libs
     ${PLUSBUILD_EXTERNAL_PROJECT_CUSTOM_COMMANDS}
