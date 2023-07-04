d029b6132dcbae36c655c37d40037a8f0b12928c
Kyle Sunderland <sunderlandkyl@gmail.com>
2023-07-04 12:21:30 -0400

    ENH: Add support for Motive 3.X

--- Modules/FindMotiveAPI.cmake
+++ Modules/FindMotiveAPI.cmake
@@ -11,7 +11,7 @@
 #  MotiveAPI_INCLUDE_DIR          The location(s) of Motive API headers
 #  MotiveAPI_MSVC80_OpenMP_DIR    Location of libiomp5md.dll needed to use Motive API
 
-SET(MotiveAPI_PATH_HINTS
+set(MotiveAPI_PATH_HINTS
   "../PLTools/OptiTrack/MotiveAPI-${MOTIVE_VERSION}"
   "../../PLTools/OptiTrack/MotiveAPI-${MOTIVE_VERSION}"
   "../trunk/PLTools/OptiTrack/MotiveAPI-${MOTIVE_VERSION}"
@@ -22,28 +22,33 @@ SET(MotiveAPI_PATH_HINTS
   "C:/Program Files/OptiTrack/Motive"
   )
 
-SET(Microsoft_VC80_OpenMP_PATH_HINTS
+set(Microsoft_VC80_OpenMP_PATH_HINTS
   "../PLTools/OptiTrack/Microsoft.VC80.OpenMP"
   "../../PLTools/OptiTrack/Microsoft.VC80.OpenMP"
   "../trunk/PLTools/OptiTrack/Microsoft.VC80.OpenMP"
   "${CMAKE_CURRENT_BINARY_DIR}/PLTools/OptiTrack/Microsoft.VC80.OpenMP"
   )
 
-find_path(MotiveAPI_DIR "/inc/NPTrackingTools.h"
-  PATHS ${MotiveAPI_PATH_HINTS}
-  DOC "OptiTrack Motive API directory")
+if (MOTIVE_VERSION VERSION_LESS 3.0.0)
+  find_path(MotiveAPI_DIR "/inc/NPTrackingTools.h"
+    PATHS ${MotiveAPI_PATH_HINTS}
+    DOC "OptiTrack Motive API directory")
+else()
+  find_path(MotiveAPI_DIR "/inc/MotiveAPI.h"
+    PATHS ${MotiveAPI_PATH_HINTS}
+    DOC "OptiTrack Motive API directory")
+endif()
 
 find_path(MotiveAPI_MSVC80_OpenMP_DIR
       NAMES vcomp.dll
       PATHS ${Microsoft_VC80_OpenMP_PATH_HINTS}
       DOC "Microsoft VC80 OpenMP directory")
 
-IF (MotiveAPI_DIR)
+if(MotiveAPI_DIR)
   set(MotiveAPI_INCLUDE_DIR ${MotiveAPI_DIR}/inc)
   mark_as_advanced(MotiveAPI_INCLUDE_DIR)
-
   set(MotiveAPI_VERSION ${MOTIVE_VERSION})
-ENDIF ()
+endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MotiveAPI
