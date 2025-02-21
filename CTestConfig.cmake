b3784098cc78cad18233b2318971c5b845067821
Kyle Sunderland <Sunderlandkyl@users.noreply.github.com>
2025-02-20 22:42:49 -0500

    DOC: Update dashboard URL to open.cdash.org

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,13 +2,13 @@
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
 ## # The following are required to uses Dart and the Cdash dashboard
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
+##   enable_testing()
+##   include(CTest)
 set(CTEST_PROJECT_NAME "PlusBuild")
 set(CTEST_BUILD_NAME "PlusBuild")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "perkdata.cs.queensu.ca")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=PlusBuild")
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "open.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=PlusBuild")
 set(CTEST_DROP_SITE_CDASH TRUE)
